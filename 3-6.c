080a964a64b07861a164de533ad09660e1c60b6e
Aayush Bajaj <aayushbajaj7@gmail.com>
2023-01-31 15:27:40 +1100

    3-2, 3-6

---
+++ 3-6.c
@@ -0,0 +1,28 @@
+/* this is a program that I would like to develop into solitaire */
+
+#include <stdio.h>
+
+/* recall that a playing card is just:
+ * a colour -> red or black
+ * a suit   -> diamond, heart, clubs or spade
+ * a number -> ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A
+ */
+
+#define     BLACK   0
+#define     RED     1
+
+#define     DIAMOND 4
+#define     CLUB    3
+#define     SPADE   2
+#define     HEART   1
+
+struct card {
+    int colour;
+    int suit;
+    int value;
+}
+
+int main(void)
+{
+    
+}
